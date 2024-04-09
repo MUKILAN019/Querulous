e3f708dee9bc873374d20e77b355975605a32a56
MUKILAN019 <mukilan.p@kalvium.community>
2024-04-09 12:02:44 +0530

    added route and mongoose connection

--- Back_End/server.js
+++ Back_End/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
+const mongoose = require("mongoose");
+require('dotenv').config();
 const port = process.env.PORT || 4444;
+const MONGO_URL = process.env.MONGO_URL;
+const cors = require("cors");
+app.use(cors());
+app.get("/",(req,res)=>{
+    res.send("Hello, World!")
+})
+mongoose.connect(MONGO_URL);
 app.listen(port, (error) => {
     if (error) {
         console.error("Server connection error:", error);
     } else {
-        console.log("Server connected successfully on port 4444");
+        console.log(`Server connected successfully on port ${port}`);
     }
 });
