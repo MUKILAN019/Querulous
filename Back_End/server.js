cee2de35f167c53670db6c63f8e817ab2c01a0dd
MUKILAN019 <mukilan.p@kalvium.community>
2024-04-18 23:49:08 +0530

    added Schema

--- Back_End/server.js
+++ Back_End/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const userRoutes = require('./Routes');
+
 require('dotenv').config();
+
 const port = process.env.PORT || 4444;
 const MONGO_URL = process.env.MONGO_URL;
-const cors = require("cors");
+
 app.use(cors());
-app.get("/",(req,res)=>{
+app.use(express.json());
+app.use('/api/user', userRoutes);
+
+app.get("/", (req, res) => {
     res.send("Hello, World!")
-})
+});
 mongoose.connect(MONGO_URL);
 app.listen(port, (error) => {
     if (error) {
