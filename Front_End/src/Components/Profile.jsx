66504e376dae9d074bdb5810468d209ab0e950ca
MUKILAN019 <mukilan.p@kalvium.community>
2024-04-29 09:27:03 +0530

    added profile UI

--- Front_End/src/Components/Profile.jsx
+++ Front_End/src/Components/Profile.jsx
@@ -2,8 +2,52 @@ import { Link } from "react-router-dom";
 
 export default function Profile() {
   return (
-    <>
-    profile
-    </>
+    <div className="p-16 bg-orange-300">
+      <div className="p-8 bg-white shadow mt-24">
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
+            <div>
+              <p className="font-bold text-gray-700 text-xl">22</p>
+              <p className="text-gray-400">Friends</p>
+            </div>
+            <div>
+              <p className="font-bold text-gray-700 text-xl">10</p>
+              <p className="text-gray-400">Photos</p>
+            </div>
+            <div>
+              <p className="font-bold text-gray-700 text-xl">89</p>
+              <p className="text-gray-400">Comments</p>
+            </div>
+          </div>
+          <div className="relative">
+            <div className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-24" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
+              </svg>
+            </div>
+          </div>
+          <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
+            <button className="text-white py-2 px-4 uppercase rounded bg-orange-400 hover:bg-orange-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+              Connect
+            </button>
+            <button className="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+              Message
+            </button>
+          </div>
+        </div>
+        <div className="mt-20 text-center border-b pb-12">
+          <h1 className="text-4xl font-medium text-gray-700">Mukilan, <span className="font-light text-gray-500">19</span></h1>
+          <p className="font-light text-gray-600 mt-3">Madurai, Tamilnadu</p>
+          <p className="mt-8 text-gray-500">Software developer</p>
+          <p className="mt-2 text-gray-500">University of KLU</p>
+        </div>
+        <div className="mt-12 flex flex-col justify-center">
+          <p className="text-gray-600 text-center font-light lg:px-16">An Computer science student; here to solve computer based problems to others</p>
+          <button className="text-indigo-500 py-2 px-4  font-medium mt-4">
+            Show more
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
