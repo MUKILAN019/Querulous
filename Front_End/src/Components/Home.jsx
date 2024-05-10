fb8625316898eed9b22999f63233410abdef8c07
MUKILAN019 <mukilan.p@kalvium.community>
2024-05-10 09:30:01 +0530

    setup basic firebase

--- Front_End/src/Components/Home.jsx
+++ Front_End/src/Components/Home.jsx
@@ -73,9 +73,9 @@ export default function HomePage() {
                 </div>
             </div>
             <div className="w-full  bg-gray-300 h-screen">
-                    <div className="w-[50%] h-screen bg-orange-300 ml-[24%]">
-                        <div className=" border border-orange-700 border-4 h-[85%] w-[90%] ml-10 bg-gray-300">
-                        <div className="grid bg-white absolute top-[20%] left-[29%] h-[70%] w-[40%]">
+                    <div className="w-[60%] h-screen bg-orange-300 ml-[20%]">
+                        <div className=" border border-orange-700 border-4 h-[85%] w-[45%] ml-[7%] top-[15%] bg-gray-300 absolute">
+                        <div className="grid bg-white absolute top-[8%] left-[5%] h-[80%] w-[90%]">
                              <div className="flex justify-between mt-[81%] bg-slate-300 w-3/4 ml-[13%]">
                              <svg className="h-8 w-8 text-orange-500 border border-dashed border-green-500 border-4"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3" /></svg>
                              <svg className="h-8 w-8 text-orange-500 border border-dashed border-red-500 border-4"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h2.67A2.31 2.31 0 0 1 22 4v7a2.31 2.31 0 0 1-2.33 2H17" /></svg>
